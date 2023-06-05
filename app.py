c204703b9db9e397e221d3436bbcca5f6c20fe5f
Sonael <sonaelneto@gmail.com>
2023-06-05 01:44:35 -0300

    Modify File List

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from werkzeug.utils import secure_filename
 import os
 import logging
+from PIL import Image
+from jinja2 import Environment
 
 app = Flask(__name__, template_folder='templates')
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+THUMBNAIL_FOLDER = os.path.join(os.getcwd(), 'thumbnails')
 app.secret_key = 'sonaelneto'  # Defina uma chave secreta para uso de sessão
+
+# Configurar o caminho para a pasta de miniaturas
+app.config['THUMBNAIL_FOLDER'] = THUMBNAIL_FOLDER
+
+
+@app.route('/thumbnails/<path:filename>')
+def serve_thumbnail(filename):
+    return send_from_directory(app.config['THUMBNAIL_FOLDER'], filename)
 
 # Configurar o arquivo de log
 log_file = "connections.log"
@@ -13,6 +24,37 @@ logging.basicConfig(filename=log_file, level=logging.INFO,
                     format='%(asctime)s %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
+
+def thumbnail_exists(filename):
+    thumbnail_filename = "thumbnail_" + filename
+    thumbnail_path = os.path.join(THUMBNAIL_FOLDER, thumbnail_filename)
+    return os.path.exists(thumbnail_path)
+
+# Verifica se o arquivo é uma imagem
+@app.template_filter('is_image_file')
+def is_image_file(filename):
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    return any(filename.lower().endswith(ext) for ext in image_extensions)
+
+# Função para gerar miniaturas das imagens
+def generate_thumbnails():
+    for filename in os.listdir(UPLOAD_FOLDER):
+        filepath = os.path.join(UPLOAD_FOLDER, filename)
+        if os.path.isfile(filepath):
+            if is_image_file(filename):
+                generate_thumbnail(filepath, filename)
+
+# Função para gerar uma miniatura de uma imagem específica
+def generate_thumbnail(filepath, filename, size=(150, 150)):
+    if thumbnail_exists(filename):
+        return
+    
+    image = Image.open(filepath)
+    image.thumbnail(size)
+    thumbnail_filename = "thumbnail_" + filename
+    thumbnail_path = os.path.join(THUMBNAIL_FOLDER, thumbnail_filename)
+    image.save(thumbnail_path)
+
 
 # Middleware para registrar as conexões
 @app.before_request
@@ -67,7 +109,7 @@ def download_file(filename):
 def file_list():
     if not session.get('logged_in'):  # Verificar se o usuário está logado antes de permitir o acesso à lista de arquivos
         return redirect(url_for('login'))
-
+    generate_thumbnails()
     files = os.listdir(UPLOAD_FOLDER)
     return render_template('filelist.html', files=files)
 
@@ -90,5 +132,6 @@ def upload():
     return "Files uploaded successfully."
 
 
+
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(port=80,debug=True)
